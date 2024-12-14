e312c158a387a3470311ac54e9271882420b071d
nannysuchawadi <147577225+nannysuchawadi@users.noreply.github.com>
2024-12-15 00:52:12 +0700

    Create Homework1.php

---
+++ 6606021621054/Homework1.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>My History</title>
+    <link rel="stylesheet" type="text/css" href="Me.css">
+</head>
+<body>
+<font size="4">
+    <header>แนะนำตัว</header>
+    <?php
+    $name = "นางสาว สุชาวดี สุขเอี่ยม";
+    $nickName = "แนน";
+    $Age = 20;
+    $StudentID = 6606021621054;
+    $Study = "เทคโนโลยีสารสนเทศ";
+    $faculty = "เทคโนโลยีและการจัดการอุตสาหกรรม";
+
+    echo "ชื่อ : " . $name . "<br/>";
+    echo "ชื่อเล่น : " . $nickName . "<br/>";
+    echo "อายุ : " . $Age . "<br/>";
+    echo "รหัสนักศึกษา : " . $StudentID . "<br/>";
+    echo "สาขา : " . $Study . "<br/>";
+    echo "คณะ : " . $faculty . "<br/>";
+    ?>
+</font>
+</body>
+</html>
